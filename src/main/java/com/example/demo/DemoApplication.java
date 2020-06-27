ca5d3ddbeaa74415a973543056c641401dee03eb
ajrockies <ajrockies@gmail.com>
2020-06-27 18:44:28 +0530

    added hello

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
