c8fc36aec467a96d6fb7d6f1a239f342c5297e40
ajrockies <ajrockies@gmail.com>
2020-06-28 17:02:17 +0530

    added rest class

---
+++ src/main/java/com/example/demo/TestClass.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test")
+public class TestClass {
+
+	@GetMapping
+	public String hello()
+	{
+		return "HI ITZ ME RIA";
+	}
+}
